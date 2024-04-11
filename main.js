68ddf8b0a3e5fe9615f871e213da5fd9b13f0879
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2024-04-11 08:42:23 -0300

    aaa

--- main.js
+++ main.js
@@ -32,5 +32,8 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
  
+    if (tempoFinal > 0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}else{
+    return "Prazo Encerrado";
 }
